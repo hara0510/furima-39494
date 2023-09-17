3880108be00a63823c4fb1aef4b847512bb5718a
hara0510 <oqj0510@gmail.com>
2023-09-17 21:10:32 +0900

    エラーメッセージの表示の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname, presence: { message: "can't be blank" }
-
-  validates :email, presence: { message: "can't be blank" }
+  validates :nickname, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, presence: { message: "can't be blank" }, format: { with: PASSWORD_REGEX, message: "is invalid. Include both letters and numbers" }
+  validates :password, format: { with: PASSWORD_REGEX }
 
-  validates :last_name, presence: { message: "can't be blank" }, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters"}
 
-  validates :first_name, presence: { message: "can't be blank" }, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
 
-  validates :last_name_kana, presence: { message: "can't be blank" }, format: { with: /\A[ァ-ヶ]+\z/, message: "invalid. Input full-width katakana characters" }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: "is invalid. Input full-width katakana characters" }
 
-  validates :first_name_kana, presence: { message: "can't be blank" }, format: { with: /\A[ァ-ヶ]+\z/, message: "invalid. Input full-width katakana characters" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: "is invalid. Input full-width katakana characters" }
 
-  validates :birthday, presence: { message: "can't be blank" }
+  validates :birthday, presence: true
 
 end
