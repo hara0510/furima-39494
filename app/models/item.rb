5466eea20239bb23c8d16b509293d5e5c468a080
hara0510 <oqj0510@gmail.com>
2023-09-23 18:13:03 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,8 @@ class Item < ApplicationRecord
   belongs_to :shippingfee
   belongs_to :prefecture
   belongs_to :deliveryday
+
+  validates :item_name, :description, presence: true
+  validates :category_id, :condition_id, :shippingfee_id, :prefecture_id, :deliveryday_id, numericality: { other_than: 1 }
+  validates :price, :presence :true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_ewual_to: 9_999_999 }
 end
