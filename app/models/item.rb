f428159c0836c41ff36a7e662e5a5866ed6d1f5d
hara0510 <oqj0510@gmail.com>
2023-09-23 17:59:10 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shippingfee
+  belongs_to :prefecture
+  belongs_to :deliveryday
 end
