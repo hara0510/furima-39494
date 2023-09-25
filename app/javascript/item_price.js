202d5e1c4758d88fa69b6c8226dd23e5dedb6ffa
hara0510 <oqj0510@gmail.com>
2023-09-25 23:17:11 +0900

    JSの設定

---
+++ app/javascript/item_price.js
@@ -0,0 +1,11 @@
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+
+  const addTaxDom = document.getElementById("add-tax-price");
+  const itemTax = Math.floor(priceInput.value * 0.1);
+  addTaxDom.innerHTML = itemTax;
+
+  const profitDom = document.getElementById("profit");
+  const itemProfit = Math.floor(priceInput.value * 0.9);
+  profitDom.innerHTML = itemProfit;
+})
