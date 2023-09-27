5deb03b4f7bcf6a68e62faf2908ba353dfdc518f
hara0510 <oqj0510@gmail.com>
2023-09-27 23:23:13 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,5 +13,5 @@ class Item < ApplicationRecord
   validates :category_id, :condition_id, :shippingfee_id, :prefecture_id, :deliveryday_id,
             numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: { message: "can't be blank" },
-                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_ewual_to: 9_999_999, message: 'is out of setting range' }, format: { with: /\A\d+\z/, message: 'is invalid. Input half-width characters' }
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }, format: { with: /\A\d+\z/, message: 'is invalid. Input half-width characters' }
 end
