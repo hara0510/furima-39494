cb5374ef58771f794cce8a525434ba71125ed4cc
hara0510 <oqj0510@gmail.com>
2023-10-07 15:30:56 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :set_item_find, only: [:show, :edit, :update]
+  before_action :set_item_find, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit]
 
 
@@ -32,6 +32,14 @@ class ItemsController < ApplicationController
       redirect_to item_path(@item.id)
     else
       render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if user_signed_in? && current_user.id == @item.user_id
+      @item.destroy
+      redirect_to root_path
+    else
     end
   end
 
