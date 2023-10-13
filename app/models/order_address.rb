67f0359d7931c91be21e816116abf0382cea3750
hara0510 <oqj0510@gmail.com>
2023-10-13 23:31:52 +0900

    バリデーションの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,17 +1,17 @@
-class DonationAddress
+class OrderAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :tel, :order_id
 
   with_options presence: true do
-    validates :user, presence:true
-    validates :item, presence:true
+    validates :user_id, presence:true
+    validates :item_id, presence:true
 
     validates :postal_code, presence: true
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city, presence: true
     validates :address, presence: true
     validates :tel, presence:true
-    validates :order, presence:true
+    validates :order_id, presence:true
   end
 
   def save
