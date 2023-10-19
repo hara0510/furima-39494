37d504e632ea0653e8bcdbca2a862f3a021d55f1
hara0510 <oqj0510@gmail.com>
2023-10-19 23:27:25 +0900

    フォームの送信設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,25 @@
 class OrdersController < ApplicationController
 
   def index
-    
   end
 
   def create
+    puts "Params: #{params.inspect}" 
 
+    @order = Order.create(order_params)
+    Address.create(address_params)
+    redirect_to root_path
+
+  end
+
+  private
+
+  def order_params
+    params.permit(:item_id).merge(user_id: current_user.id)
+  end
+
+  def address_params
+    params.permit(:postal_code, :prefecture_id, :city, :address, :building, :tel, :order_id).merge(order_id: @order.id)
   end
 
 end
