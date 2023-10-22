3f76ba96131ee72427062e08e60f3f294448b816
hara0510 <oqj0510@gmail.com>
2023-10-22 16:17:45 +0900

    ページ遷移の設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  before_action :move_to_index, only: [:index]
 
   def index
     @item = Item.find(params[:item_id])
@@ -21,4 +22,18 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :tel).merge(user_id: current_user.id, item_id: params[:item_id])
   end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    if user_signed_in?
+      if current_user.id == @item.user_id
+          redirect_to root_path
+      elsif @item.orders.any?
+        redirect_to root_path
+      end
+    else
+      redirect_to root_path
+    end
+  end
+
 end
