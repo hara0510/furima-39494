4e514ce2d920ffe4ce60b645bcb2c5e2a1bfe488
hara0510 <oqj0510@gmail.com>
2023-10-22 15:18:25 +0900

    バリデーションの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,11 +4,19 @@ class OrderAddress
 
   with_options presence: true do
     validates :user_id
-    validates :postal_code
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/,message:"is invalid. Enter it as follows (e.g. 123-4567)" }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
-    validates :tel
+    validates :tel,
+      format: {
+        with: /\A[0-9]+\z/,
+        message: "is invalid. Input only number"
+      }, 
+      length: {
+       minimum: 10, too_short: "is too short",
+       maximum: 11, too_long: "is too long"
+      }
   end
 
   def save
